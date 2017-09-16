23a4b4dc575f1d416fe00cd8785b441cf74df345
Bobby Angus <Bobby Angus>
2017-09-15 23:31:37 -0400

    changes

---
+++ functions.js
@@ -0,0 +1,14 @@
+    $("#start-button").on("click", start);  // starts the games 
+    $("#submit").on("click", finish);  // submits answers and finishes the game
+    $("#restart").on("click", restart);  // restarts the games 
+// ----------------------------------------------------------------
+// functions
+  // Hide the Question 
+  // Have the instructions and Start Button Appear
+  // After hitting start button make the questions appear and timer appear
+  // Have the contestants answers be registered by the computer
+  // Once the contestant is done have them hit the submit button
+  // Once submit button is pressed have the guess checked against answer
+  // Display how many were correct and how many were wrong
+  // If timer runs out before all questions were answered end the game
+  //
